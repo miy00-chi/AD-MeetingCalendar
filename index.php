3aef439fbef8da0f1c547757d7a4fd4380438563
Hazel Mito <hazelmito2029@gmail.com>
2025-06-23 20:22:13 +0800

    feat: Added check database connection status to index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>DATABASE CONNECTION STATUS:</h1>
+    <?php
+        include_once HANDLERS_PATH . '/mongodbChecker.handler.php';
+        include_once HANDLERS_PATH . '/postgreChecker.handler.php';
+    ?>
+</body>
+</html>

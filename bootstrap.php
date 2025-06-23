1da9f9274ac99ceb41b1c10aba5daab992d8aa95
Hazel Mito <hazelmito2029@gmail.com>
2025-06-23 20:35:23 +0800

    refractor: updated handlers_path

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,5 @@
 <?php
 define('BASE_PATH', realpath(__DIR__));
-define('HANDLERS_PATH', BASE_PATH . DIRECTORY_SEPARATOR . '/handlers' );
+define('HANDLERS_PATH', BASE_PATH . '/handlers'); 
 chdir(BASE_PATH);
+?>

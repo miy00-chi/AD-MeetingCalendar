5e0fb274ff4603756ede3e3104cbebd5781bd5af
Hazel Mito <hazelmito2029@gmail.com>
2025-06-23 21:44:37 +0800

    refractor: removed require_once for bootstrap

--- index.php
+++ index.php
@@ -8,8 +8,6 @@
 <body>
     <h1>DATABASE CONNECTION STATUS:</h1>
     <?php
-        require_once 'bootstrap.php'; // This MUST come first
-
         require_once HANDLERS_PATH . '/mongodbChecker.handler.php';
         require_once HANDLERS_PATH . '/postgreChecker.handler.php';
     ?>

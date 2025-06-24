00c3b1c8a853152bfff16e4b5c56303e1f978acb
Hazel Mito <hazelmito2029@gmail.com>
2025-06-24 11:39:46 +0800

    refractor: moved require basepath to line 4

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,7 @@
 <?php
-require BASE_PATH . '/vendor/autoload.php';
 
 define('BASE_PATH', realpath(__DIR__));
+require BASE_PATH . '/vendor/autoload.php';
 define('HANDLERS_PATH', BASE_PATH . '/handlers');
 define('UTILS_PATH', BASE_PATH . '/utils');
 chdir(BASE_PATH);
